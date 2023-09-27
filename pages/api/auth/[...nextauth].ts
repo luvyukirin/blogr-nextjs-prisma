29162e380119847ece5a7d423e52f09f0642214b
luvyukirin <lunasolcbr@gmail.com>
2023-09-27 13:23:38 +1000

    deploy

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,45 @@
-import { NextApiHandler } from 'next';
-import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import { NextApiHandler } from 'next';
+// import NextAuth from 'next-auth';
+// import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import GitHubProvider from 'next-auth/providers/github';
+// import prisma from '../../../lib/prisma';
+
+// const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+// export default authHandler;
+
+// const options = {
+//     providers: [
+//         GitHubProvider({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET,
+//         }),
+//     ],
+//     adapter: PrismaAdapter(prisma),
+//     secret: process.env.SECRET,
+// };
+
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import type { AuthOptions } from 'next-auth'
+import NextAuth from "next-auth";
 import GitHubProvider from 'next-auth/providers/github';
-import prisma from '../../../lib/prisma';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
-export default authHandler;
+const prisma = new PrismaClient();
 
-const options = {
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
     ],
-    adapter: PrismaAdapter(prisma),
     secret: process.env.SECRET,
 };
+
+
+
+const authHandler = (req: NextApiRequest, res: NextApiResponse): NextApiHandler => NextAuth(req, res, authOptions);
+
+export default authHandler;
