142fc9d3371f3cc5caf609595587d2c9ccf2aaf0
luvyukirin <lunasolcbr@gmail.com>
2023-09-27 14:49:29 +1000

    fix: ts session error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
+import { Session } from "next-auth";
 
-const App = ({ Component, pageProps }: AppProps) => {
+type MyAppProps = AppProps<{
+  session: Session;
+}>;
+
+const App = ({ Component, pageProps }: MyAppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
