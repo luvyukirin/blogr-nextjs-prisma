ab70faecb1417aeaaa574d2f0267310a7d99e789
luvyukirin <lunasolcbr@gmail.com>
2023-09-27 16:39:47 +1000

    fix post function

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,21 +1,35 @@
-import { getSession } from 'next-auth/react';
-import prisma from '../../../lib/prisma';
+// import { getSession } from 'next-auth/react';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../auth/[...nextauth]';
-import { getServerSession } from 'next-auth';
+import prisma from '../../../lib/prisma';
 
 // POST /api/post
-// Required fields in body: title
-// Optional fields in body: content
-export default async function handle(req, res) {
+// Required fields in body: title & content
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { title, content } = req.body;
 
+    // const session = await getSession({ req });
+    // console.log('session data: ', session);
+
+    // If you find that session returns null, you should consider to try
+    // using getServerSession instead of getSession.
+    // More to read: https://next-auth.js.org/getting-started/client#getsession
     const session = await getServerSession(req, res, authOptions);
     const result = await prisma.post.create({
         data: {
-            title: title,
-            content: content,
-            author: { connect: { email: session?.user?.email } },
+            title,
+            content,
+            author: {
+                connect: {
+                    email: session?.user?.email,
+                },
+            },
         },
     });
+
     res.json(result);
 }
+
+export default handler;
